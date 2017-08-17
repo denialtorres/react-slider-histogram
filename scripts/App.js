8f2949712389bf395c62039ccc13b0cf0875cb63
Daniel Torres <denial.torres@gmail.com>
2017-08-17 18:17:34 -0500

    ya arregle el puto slider

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import Histoslider from 'histoslider';
+import Histogram from './Histogram';
+
+var d3 = require('d3');
+
 
 export default class App extends Component {
+  constructor(){
+    super()
+    console.log('ESTAS EN EL PINCHE CONSTRUCTOR')
+    this.state = {
+      value: 3,
+      value1: 0,
+      value2: 2
+    };
+  }
+
+  updateSliderView(e) {
+    console.log("estas en set");
+    console.log(e[0]);
+    console.log(e[1]);
+    this.setState({value1: e[0], value2: e[1]});
+  }
   render() {
     return (
-      // Add your component markup and other subcomponent references here.
-      <h1>Hello, World!</h1>
-    );
+      <div>
+
+
+    <Histoslider
+   data={[
+     {
+       x0: 0,    // Start of the bucket
+       x: 1,     // End of the bucket
+       y: 100    // Value
+     },
+     {
+       x0: 1,    // Start of the bucket
+       x: 2,     // End of the bucket
+       y: 120    // Value
+     }
+   ]}
+   padding={20}
+   selection={[this.state.value1, this.state.value2]}
+   onChange={this.updateSliderView.bind(this)}
+ />
+    </div>);
   }
 }
